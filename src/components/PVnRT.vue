20fa39df68a82614a6d29a5c08fed8011d11e0ed
jeff-hykin <jeff.hykin@gmail.com>
2020-04-30 13:19:10 -0500

    add output system

--- src/components/PVnRT.vue
+++ src/components/PVnRT.vue
@@ -3,12 +3,24 @@
         <column >
             <pre>{{status}}</pre>
             <row >
-                <input @input="runCalc" type="text" placeholder="P (atm)" v-model="P">
-                <input @input="runCalc" type="text" placeholder="V (liters)" v-model="V">
+                <column >
+                    <input @input="runCalc" type="text" placeholder="P (atm)" v-model="P">
+                    <row >{{this.outputP}} </row>
+                </column>
+                <column >
+                    <input @input="runCalc" type="text" placeholder="V (liters)" v-model="V">
+                    <row >{{this.outputV}} </row>
+                </column>
                 =
-                <input @input="runCalc" type="text" placeholder="n (moles)" v-model="n">
+                <column >
+                    <input @input="runCalc" type="text" placeholder="n (moles)" v-model="n">
+                    <row >{{this.outputn}} </row>
+                </column>
                 R
-                <input @input="runCalc" type="text" placeholder="T (kelvin)" v-model="T">
+                <column >
+                    <input @input="runCalc" type="text" placeholder="T (kelvin)" v-model="T">
+                    <row >{{this.outputT}} </row>
+                </column>
             </row>
             <button @click="clearValues">
                 Clear
@@ -26,11 +38,18 @@ export default {
     },
     data: ()=>({
         status,
+        // constants
+        R: allUtils.R, // liter·atm/mol·K
+        // inputs
         P: "", // atm
         V: "", // liters
         n: "", // moles
-        R: 0.0821, // liter·atm/mol·K
         T: "", // kelvin
+        // outputs
+        outputP: "", // atm
+        outputV: "", // liters
+        outputn: "", // moles
+        outputT: "", // kelvin
     }),
     methods: {
         runCalc() {
@@ -39,11 +58,12 @@ export default {
             this.n = allUtils.toNumber(this.n)
             this.T = allUtils.convertToK(this.T)
             
+            // figure out which is missing and respond accordingly
             this.status = allUtils.calcHandle(this, {
-                P: ()=>(this.P = (this.n * this.R * this.T)/this.V),
-                V: ()=>(this.V = (this.n * this.R * this.T)/this.P),
-                n: ()=>(this.n = (this.P * this.V)/(this.R * this.T)),
-                T: ()=>(this.T = (this.P * this.V)/(this.n * this.R)),
+                P: ()=>(this.outputP = (this.n * this.R * this.T)/this.V),
+                V: ()=>(this.outputV = (this.n * this.R * this.T)/this.P),
+                n: ()=>(this.outputn = (this.P * this.V)/(this.R * this.T)),
+                T: ()=>(this.outputT = (this.P * this.V)/(this.n * this.R)),
             })
         },
         clearValues() {
