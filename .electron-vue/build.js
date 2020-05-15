e2e22c6b518bc7eb6171f400ce2839cf6b5bac1d
Jeff Hykin <jeff-hykin@gmail.com>
2020-05-15 13:00:51 -0500

    update packages, fix build script

--- .electron-vue/build.js
+++ .electron-vue/build.js
@@ -7,78 +7,107 @@ const chalk = require("chalk")
 const del = require("del")
 const { spawn } = require("child_process")
 const webpack = require("webpack")
-const Multispinner = require("multispinner")
 
 const mainConfig = require("./webpack.main.config")
 const rendererConfig = require("./webpack.renderer.config")
 const webConfig = require("./webpack.web.config")
 
-const doneLog = chalk.bgGreen.white(" DONE ") + " "
+const doneLog = chalk.bgGreen.white("\n DONE \n") + " "
 const errorLog = chalk.bgRed.white(" ERROR ") + " "
 const okayLog = chalk.bgBlue.white(" OKAY ") + " "
 const isCI = process.env.CI || false
+const target = process.env.BUILD_TARGET
 
-if (process.env.BUILD_TARGET === "clean") clean()
-else if (process.env.BUILD_TARGET === "web") web()
-else build()
-
-function clean() {
+//
+// clean
+//
+if (target == "clean") {
+    // remove most everything from the build folder
     del.sync(["build/*", "!build/icons", "!build/icons/icon.*"])
-    console.log(`\n${doneLog}\n`)
+    console.log(doneLog)
     process.exit()
-}
 
-function build() {
-    greeting()
-
-    del.sync(["dist/electron/*", "!.gitkeep"])
-
-    const tasks = ["main", "renderer"]
-    const m = new Multispinner(tasks, {
-        preText: "building",
-        postText: "process",
-    })
-
-    let results = ""
-
-    m.on("success", () => {
-        process.stdout.write("\x1B[2J\x1B[0f")
-        console.log(`\n\n${results}`)
-        console.log(`${okayLog}take it away ${chalk.yellow("`electron-builder`")}\n`)
+//
+// web
+//
+} else if (process.env.BUILD_TARGET === "web") {
+    del.sync(["dist/web/*", "!.gitkeep"])
+    webConfig.mode = "production"
+    webpack(webConfig, (err, stats) => {
+        if (err || stats.hasErrors()) {
+            console.log(err)
+        }
+        console.log(stats.toString({ chunks: false, colors: true }))
         process.exit()
     })
 
-    pack(mainConfig)
-        .then(result => {
-            results += result + "\n\n"
-            m.success("main")
-        })
-        .catch(err => {
-            m.error("main")
-            console.log(`\n  ${errorLog}failed to build main process`)
-            console.error(`\n${err}\n`)
-            process.exit(1)
-        })
+//
+// build
+//
+} else {
+    // 
+    // greeting
+    // 
+    const cols = process.stdout.columns
+    let text = false
 
-    pack(rendererConfig)
-        .then(result => {
-            results += result + "\n\n"
-            m.success("renderer")
+    if (cols > 85) text = "lets-build"
+    else if (cols > 60) text = "lets-|build"
+
+    if (text && !isCI) {
+        say(text, {
+            colors: ["yellow"],
+            font: "simple3d",
+            space: false,
         })
-        .catch(err => {
-            m.error("renderer")
-            console.log(`\n  ${errorLog}failed to build renderer process`)
-            console.error(`\n${err}\n`)
-            process.exit(1)
-        })
+    } else {
+        console.log(chalk.yellow.bold("\n  lets-build"))
+    }
+    console.log()
+
+    // 
+    // actual build
+    // 
+    
+    // clean up
+    del.sync(["dist/electron/*", "!.gitkeep"])
+    
+    let results = ""
+    
+    // compile main
+    pack(mainConfig).then((result) => {
+        results += result + "\n\n"
+        logResults(results)
+        console.log(`=============================`)
+        console.log(`    success building main    `)
+        console.log(`=============================`)
+    }).catch((err) => {
+        console.error(`\n${err}\n`)
+        console.log(`\n  ${errorLog}failed to build main process`)
+        process.exit(1)
+    })
+    
+    // compile renderer
+    pack(rendererConfig).then((result) => {
+        results += result + "\n\n"
+        logResults(results)
+        console.log(`===============================`)
+        console.log(`   success building renderer   `)
+        console.log(`===============================`)
+    }).catch((err) => {
+        console.error(`\n${err}\n`)
+        console.log(`\n  ${errorLog}failed to build renderer process`)
+        process.exit(1)
+    })
 }
 
 function pack(config) {
     return new Promise((resolve, reject) => {
         config.mode = "production"
         webpack(config, (err, stats) => {
-            if (err) reject(err.stack || err)
-            else if (stats.hasErrors()) {
+            if (err) {
+                reject(err.stack || err)
+            } else if (stats.hasErrors()) {
                 let err = ""
 
                 stats
@@ -87,7 +116,7 @@ function pack(config) {
                         colors: true,
                     })
                     .split(/\r?\n/)
-                    .forEach(line => {
+                    .forEach((line) => {
                         err += `    ${line}\n`
                     })
 
@@ -104,37 +133,10 @@ function pack(config) {
     })
 }
 
-function web() {
-    del.sync(["dist/web/*", "!.gitkeep"])
-    webConfig.mode = "production"
-    webpack(webConfig, (err, stats) => {
-        if (err || stats.hasErrors()) console.log(err)
-
-        console.log(
-            stats.toString({
-                chunks: false,
-                colors: true,
-            })
-        )
-
-        process.exit()
-    })
-}
-
-function greeting() {
-    const cols = process.stdout.columns
-    let text = ""
-
-    if (cols > 85) text = "lets-build"
-    else if (cols > 60) text = "lets-|build"
-    else text = false
-
-    if (text && !isCI) {
-        say(text, {
-            colors: ["yellow"],
-            font: "simple3d",
-            space: false,
-        })
-    } else console.log(chalk.yellow.bold("\n  lets-build"))
-    console.log()
+function logResults(results) {
+    console.group("")
+    console.group("")
+    results.split("\n").forEach(each=>console.log(each))
+    console.groupEnd()
+    console.groupEnd()
 }
